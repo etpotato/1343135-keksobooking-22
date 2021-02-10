3799c2af38303381f860cfc71bf4e9f79fde42cf
etpotato <mareev.pv@gmail.com>
2021-02-10 13:24:32 +0300

    Добавляет модуляцию

---
+++ js/util.js
@@ -0,0 +1,19 @@
+const getIntegerFromRange = (min, max) => {
+
+  if (min >= 0 && max >= 1 && min < max) {
+    return Math.round((Math.random() * (max - min)) + min);
+  }
+
+  throw new Error ('Invalid range');
+};
+
+const getNumberFromRange = (min, max, digits) => {
+
+  if (min >= 0 && max > min) {
+    return ((Math.random() * (max - min)) + min).toFixed(digits);
+  }
+
+  throw new Error ('Invalid range');
+};
+
+export { getIntegerFromRange, getNumberFromRange };
